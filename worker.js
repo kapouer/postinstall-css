f9c73df775c17062e30020473bf083b204efed2b
Jérémy Lal <kapouer@melix.org>
2023-01-30 13:16:32 +0100

    Update worker

--- worker.js
+++ worker.js
@@ -1,39 +1,41 @@
-const postcss = require('postcss');
+const postcss = require("postcss");
 const postcssUrl = require("postcss-url");
-const postcssImport = require('postcss-import');
-const postcssFlexBugs = require('postcss-flexbugs-fixes');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const reporter = require('postcss-reporter');
+const postcssImport = require("postcss-import");
+const postcssFlexBugs = require("postcss-flexbugs-fixes");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const reporter = require("postcss-reporter");
 
-const defaultPlugins = [
-	postcssImport({
-		plugins: [postcssUrl({url: postcssRebase})]
-	}),
-	postcssUrl({
-		url: postcssRebase
-	}),
-	postcssFlexBugs,
-	autoprefixer()
-];
+module.exports = function (input, data, output, opts) {
+	if (opts.browsers) process.env.BROWSERSLIST = opts.browsers;
 
-const nanoPlugin = cssnano({
-	preset: ['default', {
-		discardComments: {
-			removeAll: true
-		}
-	}]
-});
-
-module.exports = function(input, data, output, opts) {
-	const plugins = defaultPlugins.slice();
+	const basePlugins = [
+		postcssImport({
+			plugins: [postcssUrl({ url: postcssRebase })]
+		}),
+		postcssUrl({
+			url: postcssRebase
+		}),
+		postcssFlexBugs,
+		autoprefixer()
+	];
 	const root = postcss.parse(data, {
 		from: input,
 		map: false
 	});
 
+	const plugins = [];
+
 	if (opts.minify !== false) {
-		plugins.push(nanoPlugin);
+		plugins.push(cssnano({
+			preset: ['default', {
+				discardComments: {
+					removeAll: true
+				}
+			}]
+		}));
+	} else {
+		plugins.push(...basePlugins);
 	}
 	plugins.push(reporter);
 	return postcss(plugins).process(root, {
